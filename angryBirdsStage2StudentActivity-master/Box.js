ff5e70003387904ffcb1d590db434d9c7e9b5bd3
priyam601 <68382598+priyam601@users.noreply.github.com>
2020-08-11 21:15:23 +0530

    Add files via upload

---
+++ angryBirdsStage2StudentActivity-master/Box.js
@@ -0,0 +1,11 @@
+class Box extends BaseClass{
+  constructor(x, y, width, height) {
+
+        super(x,y,width,width);
+        this.image = loadImage("sprites/wood1.png");
+      }
+    
+    
+  }
+  
+
